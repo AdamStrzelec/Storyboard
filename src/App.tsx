f28356a3fddd06f11d5280c0d47620d416242393
strzelec <strzelec@devanddeliver.com>
2024-07-24 20:20:56 +0200

    install dependencies and default project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store, persistor } from 'src/store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
-  return <div></div>;
+	return (
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}></PersistGate>
+		</Provider>
+	);
 }
 
 export default App;
---
+++ src/store/DragAndDropStore/DragAndDropStore.ts
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const dragAndDropSlice = createSlice({
+	name: 'dragAndDrop',
+	initialState: {},
+	reducers: {},
+});
+
+export const dragAndDropSliceReducer = dragAndDropSlice.reducer;
